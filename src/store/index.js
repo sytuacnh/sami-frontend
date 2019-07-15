608d0a2d3779f03524a3d23c1c7a897d5599c9b1
Sytuacnh <ylin@trinity.edu>
2019-07-15 11:56:47 -0500

    added redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
+import createSagaMiddleware from 'redux-saga';
+import NavSaga from './sagas';
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? 
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(sagaMiddleware)
 );
+
+const store = createStore(reducer, enhancer);
+
+sagaMiddleware.run(NavSaga);
 
 export default store;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,7 +6,6 @@ const defaultState = {
 
 export default (state=defaultState, action) => {
     if (action.type === CHANGE_INPUT_VALUE) {
-        // reducer不可以修改state, 所以得深拷贝, 修改新的state
         const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState;
