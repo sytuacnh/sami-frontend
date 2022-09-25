7ca3f34c05a1678a5f98043f831aaf2262670ffb
SanAntonioMathInclude <sanantoniomathinclude@gmail.com>
2022-09-25 05:15:57 -0500

    add The First Internship

--- src/pages/events/scholarship/casting_your_future_scholarship/components/CastingYourFutureScholarshipSection.js
+++ src/pages/events/scholarship/casting_your_future_scholarship/components/CastingYourFutureScholarshipSection.js
@@ -45,14 +45,14 @@ class CastingYourFutureScholarshipSection extends PureComponent {
                   > 
                     <h2 className={classes.title}>Scholarship Description</h2>
                     <p className={classes.eventFont}>
-                      The <b>Casting Your Future Scholarship</b> is for students with low socioeconomic status to attend the Summer@SaMi Summer Camp. The summer camp is free of charge for all students. Beyond that, the Casting Your Future Scholarship will cover the transportation cost and lunch for students with low socioeconomic. 
+                      The <b>Casting Your Future Scholarship</b> is for students with low socioeconomic status to attend the Summer@SaMi Summer Camp. The summer camp is free of charge for all students. Beyond that, the Casting Your Future Scholarship will part of the students' online learning costs. 
                       <br />
                       <br />
-                      Student don’t need to apply to us. If they qualify for their school's free lunch program, they just need to email us an official document by the school that can verify this. Then the student will automatically qualify for our scholarship which covers the transportation cost and lunch in his/her camp.  
+                      Student don’t need to apply to us. If they qualify for their school's free lunch program, they just need to email us an official document by the school that can verify this. Then the student will automatically qualify for the scholarship program.  
                       <br />
                       <br />
                       More information can be found in the {" "}
-                      <Link to='/summer-at-sami-2019' target="_blank" className="link">
+                      <Link to='/summer-at-sami-2022' target="_blank" className="link">
                           <b>Summer@SaMi</b>
                       </Link>
                       {" "} Summer Camp.  
